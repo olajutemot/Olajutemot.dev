423bee7996efaf71464ee4abc0b5f098fd586860
Olajutemo Timilehin <olajutemot@gmail.com>
2024-06-27 23:15:50 -0700

    in progress

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,21 +3,23 @@ import "./About.css";
 
 const About = () => {
   return (
-    <div className="About-section">
-      <div className="outline-a"></div>
-      <img src="menu/Portfolio Headshot.jpg" alt="Timilehin" />
+    <div className="About-container">
+      <div className="About-section">
+        <div className="outline-a"></div>
+        <img src="menu/Portfolio Headshot.jpg" alt="Timilehin" />
 
-      <div className="About">
-        <h2>ABOUT ME</h2>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambl
-        </p>
-        <div className="buttons">
-          <button className="button1">Hire me</button>
-          <button className="button2">Resume</button>
+        <div className="About">
+          <h2>ABOUT ME</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambl
+          </p>
+          <div className="buttons">
+            <button className="button1">Hire me</button>
+            <button className="button2">Resume</button>
+          </div>
         </div>
       </div>
     </div>
